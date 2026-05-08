fdc90e294007303d06f425be8e9675026884e741
Aarogya Parajuli <aarogyaparajuli07@gmail.com>
2026-05-08 13:32:54 +0545

    Completed customer fetch_all, fetch_by_id, delete_by_id --without commit

--- app/api/v1/endpoints/customer.py
+++ app/api/v1/endpoints/customer.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Path , Query ,HTTPException
 from app.models.customer_model import Customer
-from app.schemas.customer import PaginatedCustomers
+from app.schemas.customer import PaginatedCustomers, customer_serializer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 from app.database.session import get_db_connection
@@ -13,7 +13,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=PaginatedCustomers)
-async def fetch_customers(
+async def fetch_all_customers(
     pagination: PaginationType,
     db: AsyncSession = Depends(get_db_connection),
     ):
@@ -35,5 +35,51 @@ async def fetch_customers(
         page_size=pagination.page_size,
         data=customers
     )
+
+@router.get("/customer")
+async def fetch_customer_by_customerNumber(
+    db: AsyncSession = Depends(get_db_connection),
+    customerNumber: int = Query(ge=1)
+    ):
+
+    result = await db.execute(
+        select(Customer)
+        .where(Customer.customerNumber == customerNumber))
+    
+    customer = result.scalar_one_or_none()
+
+    if customer is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Customer not found!"
+        )
+    
+    else:
+        return customer
+    
+@router.delete("/delete")
+async def delete_customer_by_customerNumber(
+    customerNumber: int = Query(ge=1),
+    db: AsyncSession = Depends(get_db_connection)
+    ):
+
+    result = await db.execute(
+        select(Customer)
+        .where(Customer.customerNumber == customerNumber)
+    )
+
+    customer = result.scalar_one_or_none()
+
+    if customer is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Customer not found!",
+        )
+    else:
+        await db.delete(customer)
+        # await db.commit()
+        return(
+            {"Message": f"customer with customer no. {customerNumber} deleted successfully!"}
+        )
 
 
